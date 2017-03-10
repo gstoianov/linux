ipts-linux-org/ipts-linux-new

drm/i915/gen9: support Intel Precise Touch & Stylus
This patch enables Intel Precise Touch & Stylus(IPTS) support.

IPTS requires GuC to support PCH as its client. So this patch enables GuC submission
and PCH as GuC client.

- enable GuC submission for IPTS
- redirect GT_RENDER_PIPECTL_NOTIFY_INTERRUPT to host and use it to detect a submission completed
- introduce interface to IPTS support

Signed-off-by: Hyungwoo Yang <hyungwoo.yang@intel.com>

drivers/gpu/drm/i915/i915_drv.c
@@ -49,6 +49,7 @@
 #include "i915_trace.h"
 #include "i915_vgpu.h"
 #include "intel_drv.h"
+#include "intel_ipts.h"
 
 static struct drm_driver driver;
 
@@ -632,6 +633,10 @@ static int i915_load_modeset_init(struct drm_device *dev)
 	intel_hpd_init(dev_priv);
 
 	drm_kms_helper_poll_init(dev);
+
+pr_info(">> let init ipts\n");
+	if (INTEL_GEN(dev) >= 9 && i915.enable_guc_submission)
+                intel_ipts_init(dev);
 
 	return 0;
 
@@ -1268,6 +1273,9 @@ void i915_driver_unload(struct drm_device *dev)
 {
 	struct drm_i915_private *dev_priv = to_i915(dev);
 	struct pci_dev *pdev = dev_priv->drm.pdev;
+
+	if (INTEL_GEN(dev) >= 9 && i915.enable_guc_submission)
+		intel_ipts_cleanup(dev);
 
 	intel_fbdev_fini(dev);
 

gstoianov/linux

drivers/gpu/drm/i915/i915_drv.c
@@ -49,6 +49,7 @@
 #include "i915_trace.h"
 #include "i915_vgpu.h"
 #include "intel_drv.h"
+#include "intel_ipts.h"
 
 static struct drm_driver driver;
 
@@ -623,6 +624,10 @@ static int i915_load_modeset_init(struct drm_device *dev)
 	intel_hpd_init(dev_priv);
 
 	drm_kms_helper_poll_init(dev);
+
+pr_info(">> let init ipts\n");
+	if (INTEL_GEN(dev_priv) >= 9 && i915.enable_guc_submission)
+                intel_ipts_init(dev);
 
 	return 0;
 
@@ -1278,6 +1283,9 @@ void i915_driver_unload(struct drm_device *dev)
 {
 	struct drm_i915_private *dev_priv = to_i915(dev);
 	struct pci_dev *pdev = dev_priv->drm.pdev;
+
+	if (INTEL_GEN(dev_priv) >= 9 && i915.enable_guc_submission)
+		intel_ipts_cleanup(dev);
 
 	intel_fbdev_fini(dev);
 